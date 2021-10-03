f024199617abb01801d81d73fdadbd66610aee3a
KooshaKayani <kooshaki@yahoo.com>
2021-10-03 13:40:34 +1100

    working on leavng the zone

---
+++ Pashmam Code/dev /leavinggreenzone.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env pybricks-micropython
+
+from os import system
+from pybricks.ev3devices import InfraredSensor, Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Port, Button, Color, SoundFile, Stop
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase
+from pybricks.hubs import EV3Brick 
+from pybricks.iodevices import I2CDevice
+from time import sleep
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+
+
+# Initialize the motors.
+left_motor = Motor(Port.C)
+right_motor = Motor(Port.B)
+GrabMotor = Motor(Port.A)
+LiftMotor = Motor(Port.D)
+# Initialize the sensors.
+L_line_sensor = ColorSensor(Port.S3)    
+R_line_sensor = ColorSensor(Port.S2)
+Infra = InfraredSensor(Port.S1)
+
+# Initialize the drive base. 
+robot = DriveBase(left_motor, right_motor, wheel_diameter=58, axle_track=120)
+robot.settings(turn_rate=55,straight_speed=80)
+
+robot.straight(-300)
+#to go out of the rescue zone
+while L_line_sensor.reflection() < 18 or R_line_sensor.reflection() < 18:
+	robot.drive(-70,0)
+robot.stop()
+
+robot.turn(-84)
+
+#to find the line
+while L_line_sensor.reflection() > 12 or R_line_sensor.reflection() > 12:
+	robot.drive(70,0)
+robot.stop()
+
+robot.turn(-30)
+
